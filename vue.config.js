cdc16537af0c18cfce53ced98588e8379f2353f3
jabrielm92 <122121464+jabrielm92@users.noreply.github.com>
2023-05-30 14:56:51 -0400

    updates

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/{repository-name}/'
-    : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/expense-tracker2/' : '/',
 };
